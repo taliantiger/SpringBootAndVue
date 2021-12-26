15c9652e6c402199d91d8fe129fa21a857bdda18
taliantiger <taliantiger@gmail.com>
2021-12-26 23:34:34 +0800

    add chapter01 test

---
+++ MyTest/20211225/src/main/java/myproject/App.java
@@ -0,0 +1,17 @@
+package myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//@EnableAutoConfiguration
+//@ComponentScan
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
