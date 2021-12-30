cf12c882a186ea1865ac1f13c593da0c0a98065b
taliantiger <taliantiger@gmail.com>
2021-12-30 18:40:57 +0800

    add book.java, learning Jetty setting

---
+++ MyTest/Chapter02/20211230/chapter02_1/src/main/java/myproject/App.java
@@ -0,0 +1,21 @@
+package myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//@EnableAutoConfiguration
+//@ComponentScan
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+//        SpringApplicationBuilder = new SpringApplicationBuilder(Chapter012Application.class);
+
+
+        SpringApplication.run(App.class, args);
+    }
+}
