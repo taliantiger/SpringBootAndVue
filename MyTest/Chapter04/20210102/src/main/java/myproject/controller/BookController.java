924d50e7c4c8b7351ebb85bb3c2d3a469021615f
taliantiger <taliantiger@gmail.com>
2022-01-03 10:09:06 +0800

    return Json data

---
+++ MyTest/Chapter04/20210102/src/main/java/myproject/controller/BookController.java
@@ -0,0 +1,25 @@
+package myproject.controller;
+
+import myproject.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+public class BookController {
+    @GetMapping("/book")
+    @ResponseBody
+    public Book book(){
+        Book book = new Book();
+
+        book.setAuthor("罗贯中");
+        book.setName("三国演义");
+        book.setPrice(30f);
+        book.setPublicationDate(new Date());
+
+        return book;
+    }
+
+}
