f438f865607b8f7f0d0f7791f90f497f2d6f5da2
taliantiger <taliantiger@gmail.com>
2022-01-19 18:17:26 +0800

    set AOP

---
+++ MyTest/Chapter04/20220119/4_12/src/main/java/org/sang/aop/service/Hello.java
@@ -0,0 +1,12 @@
+package org.sang.aop.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello String";
+    }
+}
