600c4350add4dc570af3757fb13e268bcd9c9709
taliantiger <taliantiger@gmail.com>
2022-01-30 16:57:45 +0800

    integrate JdbcTemplate

---
+++ MyTest/Chapter05/5_1/20220128/src/main/java/org/sang/App.java
@@ -0,0 +1,29 @@
+//package org.sang;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.stereotype.Component;
+//
+//@EnableAutoConfiguration
+//@Component
+//public class App {
+//    public static void main(String[] args) {
+//        SpringApplication.run(App.class, args);
+//    }
+//}
+
+
+
+package org.sang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class App {
+    public static void main(String[] args){
+        SpringApplication.run(App.class, args);
+    }
+}
