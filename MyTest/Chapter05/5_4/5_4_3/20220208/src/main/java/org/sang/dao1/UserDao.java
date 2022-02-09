bfc4fc2e659ef62ebef78c68d6a42fc9b2629f5c
taliantiger <taliantiger@gmail.com>
2022-02-09 16:06:52 +0800

    MultiJPA

---
+++ MyTest/Chapter05/5_4/5_4_3/20220208/src/main/java/org/sang/dao1/UserDao.java
@@ -0,0 +1,7 @@
+package org.sang.dao1;
+
+import org.sang.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+}
