3155ff270981b6bf629473b6d7bf36267f708ee1
taliantiger <taliantiger@gmail.com>
2022-02-10 19:12:00 +0800

    Redis integrate SpringBoot

---
+++ MyTest/Chapter06/6_1/20220209/src/main/java/org/sang/mybook/MyBook.java
@@ -0,0 +1,40 @@
+package org.sang.mybook;
+
+public class MyBook {
+    private Integer id;
+    private String name;
+    private String author;
+
+    @Override
+    public String toString(){
+        return "MyBook{" +
+                "id" + id +
+                ", name" + name + '\'' +
+                ", author" + author + '\'' +
+                "}";
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
