9f30047f870556e63261ca8155af120993ab231e
taliantiger <taliantiger@gmail.com>
2022-02-12 16:12:47 +0800

    Redis Cluster integrate with Spring Boot

---
+++ MyTest/Chapter06/6_1_4/20220211/src/main/java/org/sang/medel/Book.java
@@ -0,0 +1,32 @@
+package org.sang.medel;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private String name;
+    private String author;
+
+    @Override
+    public String toString(){
+        return "Book{" +
+                "name" + name + '\'' +
+                "author" + author + '\'' +
+                "}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
