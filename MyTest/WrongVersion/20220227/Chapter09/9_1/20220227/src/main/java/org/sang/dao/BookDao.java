2d91ab37cc2ab99524e92dd58a280130c9bfadf4
taliantiger <taliantiger@gmail.com>
2022-02-27 21:06:55 +0800

    WrongVersion_Chapter9_1

---
+++ MyTest/WrongVersion/20220227/Chapter09/9_1/20220227/src/main/java/org/sang/dao/BookDao.java
@@ -0,0 +1,39 @@
+package org.sang.dao;
+
+import org.sang.model.Book;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+
+@Service   // 书籍中没有@Service,作者给的代码里有Service
+
+@Repository
+@CacheConfig(cacheNames = "book_cache")
+public class BookDao {
+    @Cacheable
+    public Book getBookById(Integer id){
+        System.out.println("getBookById");
+        Book book = new Book();
+        book.setId(id);
+        book.setName("三国演义");
+        book.setAuthor("罗贯中");
+        return book;
+    }
+
+    @CachePut(key = "#book.id")
+    public Book updateBookById(Book book){
+        System.out.println("updateBookById");
+        book.setName("三国演义2");
+        return book;
+    }
+
+    @CacheEvict(key = "#id")
+    public void deleteBookById(Integer id){
+        System.out.println("deleteBookById");
+    }
+
+}
