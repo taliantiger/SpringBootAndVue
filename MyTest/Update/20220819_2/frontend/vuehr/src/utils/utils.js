47d054a95b57150b2f297fea55ac54142dc3e5cf
taliantiger <taliantiger@gmail.com>
2022-08-19 22:29:57 +0800

    try to catch auther's rate of progress

---
+++ MyTest/Update/20220819_2/frontend/vuehr/src/utils/utils.js
@@ -0,0 +1,79 @@
+import {getRequest} from './api'
+import {Message} from 'element-ui'
+
+export const isNotNullORBlank = (...args)=> {
+  for (var i = 0; i < args.length; i++) {
+    var argument = args[i];
+    if (argument == null || argument == '' || argument == undefined) {
+      Message.warning({message: '数据不能为空!'})
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
+// 初始化菜单
+export const initMenu = (router, store)=> {
+  if (store.state.routes.length > 0) {
+    return;
+  }
+  getRequest("/config/sysmenu").then(resp=> {
+    if (resp && resp.status == 200) {
+      var fmtRoutes = formatRoutes(resp.data);
+
+      // 将数据动态添加到路由中
+      router.addRoutes(fmtRoutes);
+
+      // 将数据存到store中
+      store.commit('initMenu', fmtRoutes);
+
+      // 调用connect 方法建立WebSocket 连接
+      store.dispatch('connect');
+    }
+  })
+}
+
+
+export const formatRoutes = (routes)=> {
+  let fmRoutes = [];
+  routes.forEach(router=> {
+    let {
+      path,
+      component,
+      name,
+      meta,
+      iconCls,
+      children
+    } = router;
+    if (children && children instanceof Array) {
+      children = formatRoutes(children);
+    }
+    let fmRouter = {
+      path: path,
+      component(resolve){
+        if (component.startsWith("Home")) {
+          require(['../components/' + component + '.vue'], resolve)
+        } else if (component.startsWith("Emp")) {
+          require(['../components/emp/' + component + '.vue'], resolve)
+        } else if (component.startsWith("Per")) {
+          require(['../components/personnel/' + component + '.vue'], resolve)
+        } else if (component.startsWith("Sal")) {
+          require(['../components/salary/' + component + '.vue'], resolve)
+        } else if (component.startsWith("Sta")) {
+          require(['../components/statistics/' + component + '.vue'], resolve)
+        } else if (component.startsWith("Sys")) {
+          require(['../components/system/' + component + '.vue'], resolve)
+        }
+      },
+      name: name,
+      iconCls: iconCls,
+      meta: meta,
+      children: children
+    };
+    fmRoutes.push(fmRouter);
+  })
+  return fmRoutes;
+}
